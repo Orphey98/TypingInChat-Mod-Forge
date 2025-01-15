ba7d7c62c0bae23511fc2f3fe3c2a325f30c839c
Orphey98 <stalker664@gmail.com>
2025-01-15 21:09:12 +0200

    - networking fix fo 1.20.2

--- src/main/java/me/orphey/typinginchat/networking/PacketFactory.java
+++ src/main/java/me/orphey/typinginchat/networking/PacketFactory.java
@@ -1,21 +1,18 @@
 package me.orphey.typinginchat.networking;
 
 
-import me.orphey.typinginchat.TypingInChat;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.network.ChannelBuilder;
 import net.minecraftforge.network.NetworkDirection;
 import net.minecraftforge.network.PacketDistributor;
 import net.minecraftforge.network.SimpleChannel;
 
-import java.util.function.Supplier;
-
 public class PacketFactory {
 
     private PacketFactory() {}
 
     private static final SimpleChannel INSTANCE = ChannelBuilder.named(
-            new ResourceLocation(TypingInChat.MOD_ID, "typing_status"))
+            new ResourceLocation("typinginchatmod", "typing_status"))
             .serverAcceptedVersions((status, version) -> true)
             .clientAcceptedVersions((status, version) -> true)
             .networkProtocolVersion(1)
