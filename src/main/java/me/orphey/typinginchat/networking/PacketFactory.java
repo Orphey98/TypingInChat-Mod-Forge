70ee4372696ce00592b11ed900b2c6aa79bbad96
Orphey98 <stalker664@gmail.com>
2025-01-15 21:06:42 +0200

    - 1.20.4 update - Networking update

--- src/main/java/me/orphey/typinginchat/networking/PacketFactory.java
+++ src/main/java/me/orphey/typinginchat/networking/PacketFactory.java
@@ -1,21 +1,17 @@
 package me.orphey.typinginchat.networking;
 
-
-import me.orphey.typinginchat.TypingInChat;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.network.ChannelBuilder;
 import net.minecraftforge.network.NetworkDirection;
 import net.minecraftforge.network.PacketDistributor;
 import net.minecraftforge.network.SimpleChannel;
 
-import java.util.function.Supplier;
-
 public class PacketFactory {
 
     private PacketFactory() {}
 
     private static final SimpleChannel INSTANCE = ChannelBuilder.named(
-            new ResourceLocation(TypingInChat.MOD_ID, "typing_status"))
+            new ResourceLocation("typinginchatmod", "typing_status"))
             .serverAcceptedVersions((status, version) -> true)
             .clientAcceptedVersions((status, version) -> true)
             .networkProtocolVersion(1)
@@ -25,11 +21,12 @@ public class PacketFactory {
         INSTANCE.messageBuilder(TypingPacket.class, NetworkDirection.PLAY_TO_SERVER)
                 .encoder(TypingPacket::encode)
                 .decoder(TypingPacket::decode)
-                .consumerMainThread(TypingPacket::handle);
+                .consumerMainThread(TypingPacket::handle)
+                .add();
 
     }
 
-    public static void sendToServer(Object packet) {
+    public static void sendToServer(TypingPacket packet) {
         INSTANCE.send(packet, PacketDistributor.SERVER.noArg());
     }
 }
